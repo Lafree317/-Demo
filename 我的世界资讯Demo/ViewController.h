501622c3cb0219e1d8d1478e3cb6dd53f72b093d
Lafree317 <lafree317@163.com>
2015-10-02 17:26:21 +0800

    Initial Commit

---
+++ 我的世界资讯Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  我的世界资讯Demo
+//
+//  Created by huchunyuan on 15/10/2.
+//  Copyright (c) 2015年 Lafree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 我的世界资讯Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  我的世界资讯Demo
+//
+//  Created by huchunyuan on 15/10/2.
+//  Copyright (c) 2015年 Lafree. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
